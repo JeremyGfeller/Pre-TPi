69fdb34b1bf3fb414c1b5d201579934e44b38d86
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-02-13 12:03:56 +0100

    Modification de la page contact
    J'ai traduit des bouts du formulaire, enlever un input que je n'allais pas utiliser

--- fashe-colorlib/contact.php
+++ fashe-colorlib/contact.php
@@ -58,7 +58,6 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-3 p-b-30"></div>
-
 				<div class="col-md-6 p-b-30">
 					<form class="leave-comment">
 						<h4 class="m-text26 p-b-36 p-t-15">
@@ -70,19 +69,15 @@
 						</div>
 
 						<div class="bo4 of-hidden size15 m-b-20">
-							<input class="sizefull s-text7 p-l-22 p-r-22" type="text" name="phone-number" placeholder="Phone Number">
+							<input class="sizefull s-text7 p-l-22 p-r-22" type="text" name="email" placeholder="Votre adresse e-mail">
 						</div>
 
-						<div class="bo4 of-hidden size15 m-b-20">
-							<input class="sizefull s-text7 p-l-22 p-r-22" type="text" name="email" placeholder="Email Address">
-						</div>
-
-						<textarea class="dis-block s-text7 size20 bo4 p-l-22 p-r-22 p-t-13 m-b-20" name="message" placeholder="Message"></textarea>
+						<textarea class="dis-block s-text7 size20 bo4 p-l-22 p-r-22 p-t-13 m-b-20" name="message" placeholder="Votre message"></textarea>
 
 						<div class="w-size25">
 							<!-- Button -->
 							<button class="flex-c-m size2 bg1 bo-rad-23 hov1 m-text3 trans-0-4">
-								Send
+								Envoyer
 							</button>
 						</div>
 					</form>
