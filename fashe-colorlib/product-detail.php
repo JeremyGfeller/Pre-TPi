03b906a8e910838d0b8a99048da225529648416c
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-03-02 14:45:35 +0100

    Incrémentation du fichier

--- fashe-colorlib/product-detail.php
+++ fashe-colorlib/product-detail.php
@@ -54,185 +54,161 @@
     /*echo "POST: "; print_r($_POST); echo "<br>";
     echo "SESSION: "; print_r($_SESSION); echo "<br>";
     echo "GET: "; print_r($_GET); echo "<br>";*/
-    ?>
 
-    <!-- breadcrumb -->
-	<!--div class="bread-crumb bgwhite flex-w p-l-52 p-r-15 p-t-30 p-l-15-sm">
-		<a href="index.html" class="s-text16">
-			Home
-			<i class="fa fa-angle-right m-l-8 m-r-9" aria-hidden="true"></i>
-		</a>
+    $query = "SELECT id_article, quantity, illustration, illustration1, illustration2, brand, model_name, model_prix, size, color FROM article
+                INNER JOIN model on id_model = fk_model
+                INNER JOIN size on id_size = fk_size
+                INNER JOIN color ON id_color = fk_color
+                INNER JOIN brand ON fk_brand = id_brand
+                where id_article = $articleid
+                group by id_article;"; 
 
-		<a href="product.html" class="s-text16">
-			Women
-			<i class="fa fa-angle-right m-l-8 m-r-9" aria-hidden="true"></i>
-		</a>
+    $articles = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
 
-		<a href="#" class="s-text16">
-			T-Shirt
-			<i class="fa fa-angle-right m-l-8 m-r-9" aria-hidden="true"></i>
-		</a>
+    $query2 = "SELECT id_article, quantity, size, color FROM article
+                INNER JOIN model on id_model = fk_model
+                INNER JOIN size on id_size = fk_size
+                INNER JOIN color ON id_color = fk_color
+                where id_model = $modelid;";
 
-		<span class="s-text17">
-			Boxy T-Shirt with Roll Sleeve Detail
-		</span>
-	</div-->
+    $details = $dbh->query($query2) or die ("SQL Error in:<br> $query2 <br>Error message:".$dbh->errorInfo()[2]);
 
-    <?php
-        $query = "SELECT id_article, quantity, illustration, illustration1, illustration2, brand, model_name, model_prix, size, color FROM article
-                    INNER JOIN model on id_model = fk_model
-                    INNER JOIN size on id_size = fk_size
-                    INNER JOIN color ON id_color = fk_color
-                    INNER JOIN brand ON fk_brand = id_brand
-                    where id_article = $articleid
-                    group by id_article;"; 
+    while($article = $articles->fetch()) //fetch = aller chercher
+    {
+        extract($article); // $id_article, $quantity, $illustration, $illustration1, $illustration2, $brand, $model_name, $model_prix, $size, $color
+        echo "<div class='container bgwhite p-t-35 p-b-80'>
+                <div class='flex-w flex-sb'>
+                    <div class='w-size13 p-t-30 respon5'>
+                        <div class='wrap-slick3 flex-sb flex-w'>
+                            <div class='wrap-slick3-dots'></div>
 
-        $articles = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-    
-        $query2 = "SELECT id_article, quantity, size, color FROM article
-                    INNER JOIN model on id_model = fk_model
-                    INNER JOIN size on id_size = fk_size
-                    INNER JOIN color ON id_color = fk_color
-                    where id_model = $modelid;";
-        
-        $details = $dbh->query($query2) or die ("SQL Error in:<br> $query2 <br>Error message:".$dbh->errorInfo()[2]);
-    
-        while($article = $articles->fetch()) //fetch = aller chercher
-        {
-            extract($article); // $id_article, $quantity, $illustration, $illustration1, $illustration2, $brand, $model_name, $model_prix, $size, $color
-            echo "<div class='container bgwhite p-t-35 p-b-80'>
-                    <div class='flex-w flex-sb'>
-                        <div class='w-size13 p-t-30 respon5'>
-                            <div class='wrap-slick3 flex-sb flex-w'>
-                                <div class='wrap-slick3-dots'></div>
-
-                                <div class='slick3'>
-                                    <div class='item-slick3' data-thumb='images/articles/$illustration'>
-                                        <div class='wrap-pic-w'>
-                                            <img src='images/articles/$illustration' alt='IMG-PRODUCT'>
-                                        </div>
-                                    </div>
-
-                                    <div class='item-slick3' data-thumb='images/articles/$illustration1'>
-                                        <div class='wrap-pic-w'>
-                                            <img src='images/articles/$illustration1' alt='IMG-PRODUCT'>
-                                        </div>
-                                    </div>
-
-                                    <div class='item-slick3' data-thumb='images/articles/$illustration2'>
-                                        <div class='wrap-pic-w'>
-                                            <img src='images/articles/$illustration2' alt='IMG-PRODUCT'>
-                                        </div>
+                            <div class='slick3'>
+                                <div class='item-slick3' data-thumb='images/articles/$illustration'>
+                                    <div class='wrap-pic-w'>
+                                        <img src='images/articles/$illustration' alt='IMG-PRODUCT'>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class='w-size14 p-t-30 respon5'>
-                            <h4 class='product-detail-name m-text16 p-b-13'>
-                                $brand - $model_name en $color
-                            </h4>
-
-                            <span class='m-text17'>
-                                $model_prix.-
-                            </span>
-
-                            <p class='s-text8 p-t-10'>
-                                Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
-                            </p>
-
-                            <div class='p-t-33 p-b-60'>
-                                <div class='flex-m flex-w p-b-10'>
-                                    <div class='s-text15 w-size15 t-center'>
-                                        Size
-                                    </div>
-                                        <div class='rs2-select2 rs3-select2 bo4 of-hidden w-size16'>
-                                            <select class='selection-2' name='size'>
-                                                <option>Choose an option</option>";
-            
-                                                while($detail = $details->fetch()) //fetch = aller chercher
-                                                {
-                                                    extract($detail); // $id_article, $quantity, $size, $color
-                                                    echo"<option>$size</option>";
-                                                }
-                                        echo "
-                                        </select>
-                                    </div
-                                </div>
-                                       
-                                <div class='flex-r-m flex-w p-t-10'>
-                                    <div class='w-size16 flex-m flex-w'>
-                                        <div class='flex-w bo5 of-hidden m-r-22 m-t-10 m-b-10'>
-                                            <button class='btn-num-product-down color1 flex-c-m size7 bg8 eff2'>
-                                                <i class='fs-12 fa fa-minus' aria-hidden='true'></i>
-                                            </button>
-
-                                            <input class='size8 m-text18 t-center num-product' type='number' name='num-product' value='1'>
-
-                                            <button class='btn-num-product-up color1 flex-c-m size7 bg8 eff2'>
-                                                <i class='fs-12 fa fa-plus' aria-hidden='true'></i>
-                                            </button>
-                                        </div>
-
-                                        <div class='btn-addcart-product-detail size9 trans-0-4 m-t-10 m-b-10'>
-                                            <!-- Button -->
-                                            <button class='flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4'>
-                                                Add to Cart
-                                            </button>
-                                        </div>
+                                <div class='item-slick3' data-thumb='images/articles/$illustration1'>
+                                    <div class='wrap-pic-w'>
+                                        <img src='images/articles/$illustration1' alt='IMG-PRODUCT'>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class='p-b-45'>
-                                <span class='s-text8'>Categories: Mug, Design</span>
-                            </div>
-
-                            <div class='wrap-dropdown-content bo6 p-t-15 p-b-14 active-dropdown-content'>
-                                <h5 class='js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4'>
-                                    Description
-                                    <i class='down-mark fs-12 color1 fa fa-minus dis-none' aria-hidden='true'></i>
-                                    <i class='up-mark fs-12 color1 fa fa-plus' aria-hidden='true'></i>
-                                </h5>
-
-                                <div class='dropdown-content dis-none p-t-15 p-b-23'>
-                                    <p class='s-text8'>
-                                        Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
-                                    </p>
-                                </div>
-                            </div>
-
-                            <div class='wrap-dropdown-content bo7 p-t-15 p-b-14'>
-                                <h5 class='js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4'>
-                                    Additional information
-                                    <i class='down-mark fs-12 color1 fa fa-minus dis-none' aria-hidden='true'></i>
-                                    <i class='up-mark fs-12 color1 fa fa-plus' aria-hidden='true'></i>
-                                </h5>
-
-                                <div class='dropdown-content dis-none p-t-15 p-b-23'>
-                                    <p class='s-text8'>
-                                        Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
-                                    </p>
-                                </div>
-                            </div>
-
-                            <div class='wrap-dropdown-content bo7 p-t-15 p-b-14'>
-                                <h5 class='js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4'>
-                                    Reviews (0)
-                                    <i class='down-mark fs-12 color1 fa fa-minus dis-none' aria-hidden='true'></i>
-                                    <i class='up-mark fs-12 color1 fa fa-plus' aria-hidden='true'></i>
-                                </h5>
-
-                                <div class='dropdown-content dis-none p-t-15 p-b-23'>
-                                    <p class='s-text8'>
-                                        Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
-                                    </p>
+                                <div class='item-slick3' data-thumb='images/articles/$illustration2'>
+                                    <div class='wrap-pic-w'>
+                                        <img src='images/articles/$illustration2' alt='IMG-PRODUCT'>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>";
-        }
+
+                    <div class='w-size14 p-t-30 respon5'>
+                        <h4 class='product-detail-name m-text16 p-b-13'>
+                            $brand - $model_name en $color
+                        </h4>
+
+                        <span class='m-text17'>
+                            $model_prix.-
+                        </span>
+
+                        <p class='s-text8 p-t-10'>
+                            Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
+                        </p>
+
+                        <div class='p-t-33 p-b-60'>
+                            <div class='flex-m flex-w p-b-10'>
+                                <div class='s-text15 w-size15 t-center'>
+                                    Size
+                                </div>
+                                    <div class='rs2-select2 rs3-select2 bo4 of-hidden w-size16'>
+                                        <select class='selection-2' name='size'>
+                                            <option>Choose an option</option>";
+
+                                            while($detail = $details->fetch()) //fetch = aller chercher
+                                            {
+                                                extract($detail); // $id_article, $quantity, $size, $color
+                                                echo"<option>$size</option>";
+                                            }
+                                    echo "
+                                    </select>
+                                </div
+                            </div>
+
+                            <div class='flex-r-m flex-w p-t-10'>
+                                <div class='w-size16 flex-m flex-w'>
+                                    <div class='flex-w bo5 of-hidden m-r-22 m-t-10 m-b-10'>
+                                        <button class='btn-num-product-down color1 flex-c-m size7 bg8 eff2'>
+                                            <i class='fs-12 fa fa-minus' aria-hidden='true'></i>
+                                        </button>
+
+                                        <input class='size8 m-text18 t-center num-product' type='number' name='num-product' value='1'>
+
+                                        <button class='btn-num-product-up color1 flex-c-m size7 bg8 eff2'>
+                                            <i class='fs-12 fa fa-plus' aria-hidden='true'></i>
+                                        </button>
+                                    </div>
+
+                                    <div class='btn-addcart-product-detail size9 trans-0-4 m-t-10 m-b-10'>
+                                        <!-- Button -->
+                                        <button class='flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4'>
+                                            Add to Cart
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class='p-b-45'>
+                            <span class='s-text8'>Categories: Mug, Design</span>
+                        </div>
+
+                        <div class='wrap-dropdown-content bo6 p-t-15 p-b-14 active-dropdown-content'>
+                            <h5 class='js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4'>
+                                Description
+                                <i class='down-mark fs-12 color1 fa fa-minus dis-none' aria-hidden='true'></i>
+                                <i class='up-mark fs-12 color1 fa fa-plus' aria-hidden='true'></i>
+                            </h5>
+
+                            <div class='dropdown-content dis-none p-t-15 p-b-23'>
+                                <p class='s-text8'>
+                                    Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class='wrap-dropdown-content bo7 p-t-15 p-b-14'>
+                            <h5 class='js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4'>
+                                Additional information
+                                <i class='down-mark fs-12 color1 fa fa-minus dis-none' aria-hidden='true'></i>
+                                <i class='up-mark fs-12 color1 fa fa-plus' aria-hidden='true'></i>
+                            </h5>
+
+                            <div class='dropdown-content dis-none p-t-15 p-b-23'>
+                                <p class='s-text8'>
+                                    Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class='wrap-dropdown-content bo7 p-t-15 p-b-14'>
+                            <h5 class='js-toggle-dropdown-content flex-sb-m cs-pointer m-text19 color0-hov trans-0-4'>
+                                Reviews (0)
+                                <i class='down-mark fs-12 color1 fa fa-minus dis-none' aria-hidden='true'></i>
+                                <i class='up-mark fs-12 color1 fa fa-plus' aria-hidden='true'></i>
+                            </h5>
+
+                            <div class='dropdown-content dis-none p-t-15 p-b-23'>
+                                <p class='s-text8'>
+                                    Fusce ornare mi vel risus porttitor dignissim. Nunc eget risus at ipsum blandit ornare vel sed velit. Proin gravida arcu nisl, a dignissim mauris placerat
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>";
+    }
     ?>
     
 	<!-- Footer -->
