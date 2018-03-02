058a837fd766e0e7be01b14cd9a57f425c7a2799
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-03-02 14:52:33 +0100

    Ajout de nouvelles données dans la BD

--- Database/createDatabase.sql
+++ Database/createDatabase.sql
@@ -236,9 +236,9 @@ INSERT INTO article (quantity, illustration, illustration1, illustration2, fk_co
 INSERT INTO article (quantity, illustration, illustration1, illustration2, fk_color, fk_size, fk_model) VALUES ('5', 'blazer-noir.jpg', 'blazer-noir1.jpg', 'blazer-noir2.jpg', '1', '9', '1');
 INSERT INTO article (quantity, illustration, illustration1, illustration2, fk_color, fk_size, fk_model) VALUES ('5', 'blazer-noir.jpg', 'blazer-noir1.jpg', 'blazer-noir2.jpg', '1', '10', '1');
 INSERT INTO article (quantity, illustration, illustration1, illustration2, fk_color, fk_size, fk_model) VALUES ('5', 'blazer-noir.jpg', 'blazer-noir1.jpg', 'blazer-noir2.jpg', '1', '11', '1');
-INSERT INTO article (quantity, illustration, fk_model, fk_size, fk_color) VALUES ('5', 'blazer-blanche-low.jpg', '2', '9', '2');
-INSERT INTO article (quantity, illustration, fk_model, fk_size, fk_color) VALUES ('5', 'blazer-blanche-low.jpg', '2', '10', '2');
-INSERT INTO article (quantity, illustration, fk_model, fk_size, fk_color) VALUES ('5', 'blazer-blanche-low.jpg', '2', '11', '2');
-INSERT INTO article (quantity, illustration, fk_model, fk_size, fk_color) VALUES ('5', 'classic-noir.jpg', '4', '1', '1');
+INSERT INTO article (quantity, illustration, illustration1, illustration2, fk_color, fk_size, fk_model) VALUES ('5', 'blazer-blanche-low.jpg', 'blazer-blanche-low1.jpg', 'blazer-blanche-low2.jpg', '2', '9', '2');
+INSERT INTO article (quantity, illustration, illustration1, illustration2, fk_color, fk_size, fk_model) VALUES ('5', 'blazer-blanche-low.jpg', 'blazer-blanche-low1.jpg', 'blazer-blanche-low2.jpg', '2', '10', '2');
+INSERT INTO article (quantity, illustration, illustration1, illustration2, fk_color, fk_size, fk_model) VALUES ('5', 'blazer-blanche-low.jpg', 'blazer-blanche-low1.jpg', 'blazer-blanche-low2.jpg', '2', '11', '2');
+INSERT INTO article (quantity, illustration, illustration1, illustration2, fk_color, fk_size, fk_model) VALUES ('5', 'classic-noir.jpg', 'classic-noir1.jpg', 'classic-noir2.jpg', '4', '1', '1');
 INSERT INTO article (quantity, illustration, fk_model, fk_size, fk_color) VALUES ('5', 'herschel-gris.jpg', '5', '12', '6');
 INSERT INTO article (quantity, illustration, fk_model, fk_size, fk_color) VALUES ('5', 'g-star-bleu.jpg', '6', '18', '5');
