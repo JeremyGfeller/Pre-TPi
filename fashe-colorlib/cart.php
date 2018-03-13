217d0776089a7a571e260dcd1c61b835e345cb4c
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-03-13 14:11:02 +0100

    Ajout d'une fonction
    Elle permet de savoir si un utilisateur est connecté et s'il peut consulter son panier.

--- fashe-colorlib/cart.php
+++ fashe-colorlib/cart.php
@@ -64,7 +64,9 @@
 	<!-- Cart -->
     <?php
     
-    $query = "SELECT id_basket, id_users, id_article, id_orderlist, size, illustration, orderlist.quantity, users_lastName, users_role, model_name, model_prix, brand FROM basket
+    if(isset($UserName))
+    {
+        $query = "SELECT id_basket, id_users, id_article, id_orderlist, size, illustration, orderlist.quantity, users_lastName, users_role, model_name, model_prix, brand FROM basket
               inner join orderlist on fk_basket = id_basket
               inner join users on fk_users = id_users
               inner join article on fk_article = id_article
@@ -73,176 +75,103 @@
               inner join brand on fk_brand = id_brand
               WHERE fk_users = $IDPersonne;";
     
-    $baskets = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-    
-    echo"
-	<section class=cart bgwhite p-t-70 p-b-100'>
-		<div class='container'>
-			<!-- Cart item -->
-			<div class='container-table-cart pos-relative'>
-				<div class='wrap-table-shopping-cart bgwhite'>
-					<table class='table-shopping-cart'>
-						<tr class='table-head'>
-							<th class='column-1'></th>
-							<th class='column-2'>Product</th>
-							<th class='column-3'>Price</th>
-							<th class='column-3'>Taille</th>
-							<th class='column-4'>Quantity</th>
-							<th class='column-3'>Supprimer</th>
-						</tr>";
-    
-                        while($basket = $baskets->fetch())
-                        {
-                            extract($basket); // $id_basket, $id_users, $id_article, $id_orderlist, $users_firstName, $size, $illustration, $orderlist.quantity, $users_lastName, $users_role, $model_name, $model_prix, $brand
+        $baskets = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
+
+        echo"
+        <section class=cart bgwhite p-t-70 p-b-100'>
+            <div class='container'>
+                <!-- Cart item -->
+                <div class='container-table-cart pos-relative'>
+                    <div class='wrap-table-shopping-cart bgwhite'>
+                        <table class='table-shopping-cart'>
+                            <tr class='table-head'>
+                                <th class='column-1'></th>
+                                <th class='column-2'>Product</th>
+                                <th class='column-3'>Price</th>
+                                <th class='column-3'>Taille</th>
+                                <th class='column-4'>Quantity</th>
+                                <th class='column-3'>Supprimer</th>
+                            </tr>";
+
+                            while($basket = $baskets->fetch())
+                            {
+                                extract($basket); // $id_basket, $id_users, $id_article, $id_orderlist, $users_firstName, $size, $illustration, $orderlist.quantity, $users_lastName, $users_role, $model_name, $model_prix, $brand
+                                echo "
+                                <tr class='table-row'>
+                                    <td class='column-1'>
+                                        <div class='cart-img-product b-rad-4 o-f-hidden'>
+                                            <img src='images/articles/$illustration' alt='IMG-PRODUCT'>
+                                        </div>
+                                    </td>
+                                    <td class='column-2'>$brand - $model_name</td>
+                                    <td class='column-3'>$model_prix.-</td>
+                                    <td class='column-3'>$size</td>
+                                    <td class='column-4'>
+                                        <div class='flex-w bo5 of-hidden w-size17'>
+                                            <button class='btn-num-product-down color1 flex-c-m size7 bg8 eff2'>
+                                                <i class='fs-12 fa fa-minus' aria-hidden='true'></i>
+                                            </button>
+
+                                            <input class='size8 m-text18 t-center num-product' type='number' name='num-product1' value='1'>
+
+                                            <button class='btn-num-product-up color1 flex-c-m size7 bg8 eff2'>
+                                                <i class='fs-12 fa fa-plus' aria-hidden='true'></i>
+                                            </button>
+                                        </div>
+                                    </td>
+                                    <td class='column-5'>
+                                        <form method='post'>
+                                            <button type='submit' name='deleteBasket' value='$id_article'>
+                                                <img src='images/icons/effacer.png' alt='IMG-PRODUCT'> 
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>";
+                            }
+
+                            $query = "SELECT id_orderlist, orderlist.quantity, fk_article, fk_basket, sum(model_prix) as total FROM yonik.orderlist
+                                      inner join article on fk_article = id_article
+                                      inner join model on fk_model = id_model
+                                      inner join basket on fk_basket = id_basket
+                                      where fk_users = (select fk_users from basket where fk_users = $IDPersonne);";
+                            $additions = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
+
+                            if ($additions->rowCount() > 0)
+                            {
+                                $addition = $additions->fetch(); 
+                                extract($addition); //$id_users, $users_firstName, $users_lastName, $users_role, $users_login, $users_password, $hashPassword
+                            }
                             echo "
                             <tr class='table-row'>
-                                <td class='column-1'>
-                                    <div class='cart-img-product b-rad-4 o-f-hidden'>
-                                        <img src='images/articles/$illustration' alt='IMG-PRODUCT'>
-                                    </div>
-                                </td>
-                                <td class='column-2'>$brand - $model_name</td>
-                                <td class='column-3'>$model_prix.-</td>
-                                <td class='column-3'>$size</td>
-                                <td class='column-4'>
-                                    <div class='flex-w bo5 of-hidden w-size17'>
-                                        <button class='btn-num-product-down color1 flex-c-m size7 bg8 eff2'>
-                                            <i class='fs-12 fa fa-minus' aria-hidden='true'></i>
-                                        </button>
-
-                                        <input class='size8 m-text18 t-center num-product' type='number' name='num-product1' value='1'>
-
-                                        <button class='btn-num-product-up color1 flex-c-m size7 bg8 eff2'>
-                                            <i class='fs-12 fa fa-plus' aria-hidden='true'></i>
-                                        </button>
-                                    </div>
-                                </td>
-                                <td class='column-5'>
-                                    <form method='post'>
-                                        <button type='submit' name='deleteBasket' value='$id_article'>
-                                            <img src='images/icons/effacer.png' alt='IMG-PRODUCT'> 
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>";
-                        }
-                            
-                        $query = "SELECT id_orderlist, orderlist.quantity, fk_article, fk_basket, sum(model_prix) as total FROM yonik.orderlist
-                                  inner join article on fk_article = id_article
-                                  inner join model on fk_model = id_model
-                                  inner join basket on fk_basket = id_basket
-                                  where fk_users = (select fk_users from basket where fk_users = $IDPersonne);";
-                        $additions = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-    
-                        if ($additions->rowCount() > 0)
-                        {
-                            $addition = $additions->fetch(); 
-                            extract($addition); //$id_users, $users_firstName, $users_lastName, $users_role, $users_login, $users_password, $hashPassword
-                        }
-                        echo "
-                        <tr class='table-row'>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <th class='column-2'>
-                                TOTAL
-                            </th>";
-                            if($total != 0)
-                            {
-                                echo "<td>$total.-</td>";
-                            }
-                            else
-                            {
-                                echo "<td>0.-</td>";
-                            }
-                        echo "
-                        </tr>
-					</table>
-				</div>
-			</div>
-	</section>";
-    
-    /*
-    
-    <!-- Total -->
-			<div class='bo9 w-size18 p-l-40 p-r-40 p-t-30 p-b-38 m-t-30 m-r-0 m-l-auto p-lr-15-sm'>
-				<h5 class='m-text20 p-b-24'>
-					Cart Totals
-				</h5>
-
-				<!--  -->
-				<div class='flex-w flex-sb-m p-b-12'>
-					<span class='s-text18 w-size19 w-full-sm'>
-						Subtotal:
-					</span>
-
-					<span class='m-text21 w-size20 w-full-sm'>
-						$39.00
-					</span>
-				</div>
-
-				<!--  -->
-				<div class='flex-w flex-sb bo10 p-t-15 p-b-20'>
-					<span class='s-text18 w-size19 w-full-sm'>
-						Shipping:
-					</span>
-
-					<div class='w-size20 w-full-sm'>
-						<p class='s-text8 p-b-23'>
-							There are no shipping methods available. Please double check your address, or contact us if you need any help.
-						</p>
-
-						<span class='s-text19'>
-							Calculate Shipping
-						</span>
-
-						<div class='rs2-select2 rs3-select2 rs4-select2 bo4 of-hidden w-size21 m-t-8 m-b-12'>
-							<select class='selection-2' name='country'>
-								<option>Select a country...</option>
-								<option>US</option>
-								<option>UK</option>
-								<option>Japan</option>
-							</select>
-						</div>
-
-						<div class='size13 bo4 m-b-12'>
-						<input class='sizefull s-text7 p-l-15 p-r-15' type='text' name='state' placeholder='State /  country'>
-						</div>
-
-						<div class='size13 bo4 m-b-22'>
-							<input class='sizefull s-text7 p-l-15 p-r-15' type='text' name='postcode' placeholder='Postcode / Zip'>
-						</div>
-
-						<div class='size14 trans-0-4 m-b-10'>
-							<!-- Button -->
-							<button class='flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4'>
-								Update Totals
-							</button>
-						</div>
-					</div>
-				</div>
-
-				<!--  -->
-				<div class='flex-w flex-sb-m p-t-26 p-b-30'>
-					<span class='m-text22 w-size19 w-full-sm'>
-						Total:
-					</span>
-
-					<span class='m-text21 w-size20 w-full-sm'>
-						$39.00
-					</span>
-				</div>
-
-				<div class='size15 trans-0-4'>
-					<!-- Button -->
-					<button class='flex-c-m sizefull bg1 bo-rad-23 hov1 s-text1 trans-0-4'>
-						Proceed to Checkout
-					</button>
-				</div>
-			</div>
-		</div>
-    
-    */
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <th class='column-2'>
+                                    TOTAL
+                                </th>";
+                                if($total != 0)
+                                {
+                                    echo "<td>$total.-</td>";
+                                }
+                                else
+                                {
+                                    echo "<td>0.-</td>";
+                                }
+                            echo "
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+        </section>";
+    }
+    else
+    {
+        echo "<section style='padding: 10px;'>
+                    <h2 class='t-center'>
+                        Merci de vous connecter pour accéder à cette page  
+                    </h2>
+                  </section>";
+    }
     ?>
     
     <br><br>
