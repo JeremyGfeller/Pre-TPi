9b70b6c3d5612dfb8b800f4b35dd8455ee4af4ce
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-03-13 11:33:18 +0100

    Affiche le contenu du panier d'un utilisateur
    Quand on clique sur l'icône en haut à droite il affiche la panier de l'utilisateur s'il est connecté

--- fashe-colorlib/cart.php
+++ fashe-colorlib/cart.php
@@ -91,41 +91,41 @@
 							<th class='column-3'>Supprimer</th>
 						</tr>";
     
-                            while($basket = $baskets->fetch())
-                            {
-                                extract($basket); // $id_basket, $id_users, $id_article, $id_orderlist, $users_firstName, $size, $illustration, $orderlist.quantity, $users_lastName, $users_role, $model_name, $model_prix, $brand
-                                echo "
-                                <tr class='table-row'>
-                                    <td class='column-1'>
-                                        <div class='cart-img-product b-rad-4 o-f-hidden'>
-                                            <img src='images/articles/$illustration' alt='IMG-PRODUCT'>
-                                        </div>
-                                    </td>
-                                    <td class='column-2'>$brand - $model_name</td>
-                                    <td class='column-3'>$model_prix.-</td>
-                                    <td class='column-3'>$size</td>
-                                    <td class='column-4'>
-                                        <div class='flex-w bo5 of-hidden w-size17'>
-                                            <button class='btn-num-product-down color1 flex-c-m size7 bg8 eff2'>
-                                                <i class='fs-12 fa fa-minus' aria-hidden='true'></i>
-                                            </button>
+                        while($basket = $baskets->fetch())
+                        {
+                            extract($basket); // $id_basket, $id_users, $id_article, $id_orderlist, $users_firstName, $size, $illustration, $orderlist.quantity, $users_lastName, $users_role, $model_name, $model_prix, $brand
+                            echo "
+                            <tr class='table-row'>
+                                <td class='column-1'>
+                                    <div class='cart-img-product b-rad-4 o-f-hidden'>
+                                        <img src='images/articles/$illustration' alt='IMG-PRODUCT'>
+                                    </div>
+                                </td>
+                                <td class='column-2'>$brand - $model_name</td>
+                                <td class='column-3'>$model_prix.-</td>
+                                <td class='column-3'>$size</td>
+                                <td class='column-4'>
+                                    <div class='flex-w bo5 of-hidden w-size17'>
+                                        <button class='btn-num-product-down color1 flex-c-m size7 bg8 eff2'>
+                                            <i class='fs-12 fa fa-minus' aria-hidden='true'></i>
+                                        </button>
 
-                                            <input class='size8 m-text18 t-center num-product' type='number' name='num-product1' value='1'>
+                                        <input class='size8 m-text18 t-center num-product' type='number' name='num-product1' value='1'>
 
-                                            <button class='btn-num-product-up color1 flex-c-m size7 bg8 eff2'>
-                                                <i class='fs-12 fa fa-plus' aria-hidden='true'></i>
-                                            </button>
-                                        </div>
-                                    </td>
-                                    <td class='column-5'>
-                                        <form method='post'>
-                                            <button type='submit' name='deleteBasket' value='$id_article'>
-                                                <img src='images/icons/effacer.png' alt='IMG-PRODUCT'> 
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>";
-                            }
+                                        <button class='btn-num-product-up color1 flex-c-m size7 bg8 eff2'>
+                                            <i class='fs-12 fa fa-plus' aria-hidden='true'></i>
+                                        </button>
+                                    </div>
+                                </td>
+                                <td class='column-5'>
+                                    <form method='post'>
+                                        <button type='submit' name='deleteBasket' value='$id_article'>
+                                            <img src='images/icons/effacer.png' alt='IMG-PRODUCT'> 
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>";
+                        }
                             
                         $query = "SELECT id_orderlist, orderlist.quantity, fk_article, fk_basket, sum(model_prix) as total FROM yonik.orderlist
                                   inner join article on fk_article = id_article
