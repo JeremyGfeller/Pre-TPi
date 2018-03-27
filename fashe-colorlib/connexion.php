d4579e9c4b76769e167959cfc72cb5c5c26366d8
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-03-27 09:21:00 +0200

    Check à la connexion ou l'inscription
    Lors de la connexion il contrôle si le mot de passe et le login sont valides. Lors de l'iscription il regarde dans la base de données si le pseudo n'éxiste pas, s'il éxiste un message apparaît.

--- fashe-colorlib/connexion.php
+++ fashe-colorlib/connexion.php
@@ -38,27 +38,42 @@
 
     require_once('fonctions.php');
     ConnectDB();
+	require_once('top.php');
 
     if(isset($_POST['firstName']))
     {
-        extract($_POST);
-        $query = "INSERT INTO users (users_firstName, users_lastName, users_role, users_login, users_password) VALUES ('$firstName', '$lastName', '$role', '$login', password('$password'));";  
-        $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-		
-		$query = "SELECT id_users, users_firstName, users_lastName, users_role, users_login, users_password, password('$password') as hashPassword FROM users where users_login = '$login';";
-        $connexions = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
+		extract($_POST);
 
-        if ($connexions->rowCount() > 0)
+		$query = "SELECT id_users, users_firstName, users_lastName, users_role, users_login, users_password, password('$password') as hashPassword FROM users where users_login = '$login';";
+		$checkconnexion = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
+		
+		if ($checkconnexion->rowCount() > 0)
         {
-            $connexion = $connexions->fetch(); 
-			extract($connexion); //$id_users, $users_firstName, $users_lastName, $users_role, $users_login, $users_password, $hashPassword
-			$_SESSION['IDPersonne'] = $id_users;
-			$_SESSION['UserName'] = "$firstName $lastName";
-			$_SESSION['role'] = $role;
+            echo "<section style='padding-top: 10px;'>
+                    <h2 class='t-center'>
+                        Ce pseudo éxiste déjà 
+                    </h2>
+                  </section>";
 		}
-    }
-    
-    if(isset($_POST['login']))
+		else
+		{
+			$query = "INSERT INTO users (users_firstName, users_lastName, users_role, users_login, users_password) VALUES ('$firstName', '$lastName', '$role', '$login', password('$password'));";  
+			$dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
+
+			$query = "SELECT id_users, users_firstName, users_lastName, users_role, users_login, users_password, password('$password') as hashPassword FROM users where users_login = '$login';";
+			$connexions = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
+	
+			if ($connexions->rowCount() > 0)
+			{
+				$connexion = $connexions->fetch(); 
+				extract($connexion); //$id_users, $users_firstName, $users_lastName, $users_role, $users_login, $users_password, $hashPassword
+				$_SESSION['IDPersonne'] = $id_users;
+				$_SESSION['UserName'] = "$firstName $lastName";
+				$_SESSION['role'] = $role;
+			}
+		}
+    }   
+    else if(isset($_POST['login']))
     {
         $login = $_POST['login'];
         $password = $_POST['password'];
@@ -77,260 +92,256 @@
 				$_SESSION['UserName'] = "$users_firstName $users_lastName";
 				$_SESSION['role'] = $users_role;
 			}
-		}
-		else
-		{
-			echo "<section style='margin-top: 150px;'>
-					<h2 class='t-center'>
-						Login ou mot de passe faux 
-					</h2>
-				</section>";
+			else
+			{
+				echo "<section style='margin-top: 10px;'>
+						<h2 class='t-center'>
+							Login ou mot de passe faux 
+						</h2>
+					</section>";
+			}
 		}
     }
     ?>
-<body class="animsition">
-
-	<!-- Header -->
-	<?php require_once('top.php'); ?>
-
-	<!-- content page -->
-	<section class="bgwhite p-t-60">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12 col-lg-12 p-b-75">
-					<div class="p-r-50 p-r-0-lg">
-                        <?php	
-                            if(!isset($_SESSION['IDPersonne']))
-                            {
-                                echo "<div class='item-blog p-b-80 t-center'>
-                                    <h2>Connexion</h2><br>";
-                                        echo loginBox();
-                                echo "</div>                            
-                                <div class='item-blog p-b-80 t-center'>
-                                    <h3>Inscription</h3><br>";
-                                        echo inscription();
-                                echo"</div>";
-                            }
-                            else
-                            {
-                                echo "<div class='item-blog p-b-80 t-center'>
-                                    <h2>Connexion réussie</h2><br>";
-                                        echo loginBox();
-                                echo "</div>";
-                            }
-                        ?>
-				    </div>
-			     </div>
-            </div>
-		</div>
-	</section>
-
-
-	<!-- Footer -->
-	<footer class="bg6 p-t-45 p-b-43 p-l-45 p-r-45">
-		<div class="flex-w p-b-90">
-			<div class="w-size6 p-t-30 p-l-15 p-r-15 respon3">
-				<h4 class="s-text12 p-b-30">
-					GET IN TOUCH
-				</h4>
-
-				<div>
-					<p class="s-text7 w-size27">
-						Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
-					</p>
-
-					<div class="flex-m p-t-30">
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-facebook"></a>
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-instagram"></a>
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-pinterest-p"></a>
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-snapchat-ghost"></a>
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-youtube-play"></a>
+	<body class="animsition">
+		<!-- content page -->
+		<section class="bgwhite p-t-60">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-lg-12 p-b-75">
+						<div class="p-r-50 p-r-0-lg">
+							<?php	
+								if(!isset($_SESSION['IDPersonne']))
+								{
+									echo "<div class='item-blog p-b-80 t-center'>
+										<h2>Connexion</h2><br>";
+											echo loginBox();
+									echo "</div>                            
+									<div class='item-blog p-b-80 t-center'>
+										<h3>Inscription</h3><br>";
+											echo inscription();
+									echo"</div>";
+								}
+								else
+								{
+									echo "<div class='item-blog p-b-80 t-center'>
+										<h2>Connexion réussie</h2><br>";
+											echo loginBox();
+									echo "</div>";
+								}
+							?>
+						</div>
 					</div>
 				</div>
 			</div>
+		</section>
 
-			<div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
-				<h4 class="s-text12 p-b-30">
-					Categories
-				</h4>
 
-				<ul>
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Men
-						</a>
-					</li>
+		<!-- Footer -->
+		<footer class="bg6 p-t-45 p-b-43 p-l-45 p-r-45">
+			<div class="flex-w p-b-90">
+				<div class="w-size6 p-t-30 p-l-15 p-r-15 respon3">
+					<h4 class="s-text12 p-b-30">
+						GET IN TOUCH
+					</h4>
 
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Women
-						</a>
-					</li>
+					<div>
+						<p class="s-text7 w-size27">
+							Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
+						</p>
 
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Dresses
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Sunglasses
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
-				<h4 class="s-text12 p-b-30">
-					Links
-				</h4>
-
-				<ul>
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Search
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							About Us
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Contact Us
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Returns
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
-				<h4 class="s-text12 p-b-30">
-					Help
-				</h4>
-
-				<ul>
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Track Order
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Returns
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Shipping
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							FAQs
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div class="w-size8 p-t-30 p-l-15 p-r-15 respon3">
-				<h4 class="s-text12 p-b-30">
-					Newsletter
-				</h4>
-
-				<form>
-					<div class="effect1 w-size9">
-						<input class="s-text7 bg6 w-full p-b-5" type="text" name="email" placeholder="email@example.com">
-						<span class="effect1-line"></span>
+						<div class="flex-m p-t-30">
+							<a href="#" class="fs-18 color1 p-r-20 fa fa-facebook"></a>
+							<a href="#" class="fs-18 color1 p-r-20 fa fa-instagram"></a>
+							<a href="#" class="fs-18 color1 p-r-20 fa fa-pinterest-p"></a>
+							<a href="#" class="fs-18 color1 p-r-20 fa fa-snapchat-ghost"></a>
+							<a href="#" class="fs-18 color1 p-r-20 fa fa-youtube-play"></a>
+						</div>
 					</div>
+				</div>
 
-					<div class="w-size2 p-t-20">
-						<!-- Button -->
-						<button class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
-							Subscribe
-						</button>
-					</div>
+				<div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
+					<h4 class="s-text12 p-b-30">
+						Categories
+					</h4>
 
-				</form>
+					<ul>
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Men
+							</a>
+						</li>
+
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Women
+							</a>
+						</li>
+
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Dresses
+							</a>
+						</li>
+
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Sunglasses
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
+					<h4 class="s-text12 p-b-30">
+						Links
+					</h4>
+
+					<ul>
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Search
+							</a>
+						</li>
+
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								About Us
+							</a>
+						</li>
+
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Contact Us
+							</a>
+						</li>
+
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Returns
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
+					<h4 class="s-text12 p-b-30">
+						Help
+					</h4>
+
+					<ul>
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Track Order
+							</a>
+						</li>
+
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Returns
+							</a>
+						</li>
+
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								Shipping
+							</a>
+						</li>
+
+						<li class="p-b-9">
+							<a href="#" class="s-text7">
+								FAQs
+							</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="w-size8 p-t-30 p-l-15 p-r-15 respon3">
+					<h4 class="s-text12 p-b-30">
+						Newsletter
+					</h4>
+
+					<form>
+						<div class="effect1 w-size9">
+							<input class="s-text7 bg6 w-full p-b-5" type="text" name="email" placeholder="email@example.com">
+							<span class="effect1-line"></span>
+						</div>
+
+						<div class="w-size2 p-t-20">
+							<!-- Button -->
+							<button class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
+								Subscribe
+							</button>
+						</div>
+
+					</form>
+				</div>
 			</div>
+
+			<div class="t-center p-l-15 p-r-15">
+				<a href="#">
+					<img class="h-size2" src="images/icons/paypal.png" alt="IMG-PAYPAL">
+				</a>
+
+				<a href="#">
+					<img class="h-size2" src="images/icons/visa.png" alt="IMG-VISA">
+				</a>
+
+				<a href="#">
+					<img class="h-size2" src="images/icons/mastercard.png" alt="IMG-MASTERCARD">
+				</a>
+
+				<a href="#">
+					<img class="h-size2" src="images/icons/express.png" alt="IMG-EXPRESS">
+				</a>
+
+				<a href="#">
+					<img class="h-size2" src="images/icons/discover.png" alt="IMG-DISCOVER">
+				</a>
+
+				<div class="t-center s-text8 p-t-20">
+					Copyright © 2018 All rights reserved. | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+				</div>
+			</div>
+		</footer>
+
+
+
+		<!-- Back to top -->
+		<div class="btn-back-to-top bg0-hov" id="myBtn">
+			<span class="symbol-btn-back-to-top">
+				<i class="fa fa-angle-double-up" aria-hidden="true"></i>
+			</span>
 		</div>
 
-		<div class="t-center p-l-15 p-r-15">
-			<a href="#">
-				<img class="h-size2" src="images/icons/paypal.png" alt="IMG-PAYPAL">
-			</a>
-
-			<a href="#">
-				<img class="h-size2" src="images/icons/visa.png" alt="IMG-VISA">
-			</a>
-
-			<a href="#">
-				<img class="h-size2" src="images/icons/mastercard.png" alt="IMG-MASTERCARD">
-			</a>
-
-			<a href="#">
-				<img class="h-size2" src="images/icons/express.png" alt="IMG-EXPRESS">
-			</a>
-
-			<a href="#">
-				<img class="h-size2" src="images/icons/discover.png" alt="IMG-DISCOVER">
-			</a>
-
-			<div class="t-center s-text8 p-t-20">
-				Copyright © 2018 All rights reserved. | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-			</div>
-		</div>
-	</footer>
+		<!-- Container Selection -->
+		<div id="dropDownSelect1"></div>
+		<div id="dropDownSelect2"></div>
 
 
 
-	<!-- Back to top -->
-	<div class="btn-back-to-top bg0-hov" id="myBtn">
-		<span class="symbol-btn-back-to-top">
-			<i class="fa fa-angle-double-up" aria-hidden="true"></i>
-		</span>
-	</div>
+	<!--===============================================================================================-->
+		<script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<!--===============================================================================================-->
+		<script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
+	<!--===============================================================================================-->
+		<script type="text/javascript" src="vendor/bootstrap/js/popper.js"></script>
+		<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<!--===============================================================================================-->
+		<script type="text/javascript" src="vendor/select2/select2.min.js"></script>
+		<script type="text/javascript">
+			$(".selection-1").select2({
+				minimumResultsForSearch: 20,
+				dropdownParent: $('#dropDownSelect1')
+			});
 
-	<!-- Container Selection -->
-	<div id="dropDownSelect1"></div>
-	<div id="dropDownSelect2"></div>
+			$(".selection-2").select2({
+				minimumResultsForSearch: 20,
+				dropdownParent: $('#dropDownSelect2')
+			});
+		</script>
+	<!--===============================================================================================-->
+		<script src="js/main.js"></script>
 
-
-
-<!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/bootstrap/js/popper.js"></script>
-	<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/select2/select2.min.js"></script>
-	<script type="text/javascript">
-		$(".selection-1").select2({
-			minimumResultsForSearch: 20,
-			dropdownParent: $('#dropDownSelect1')
-		});
-
-		$(".selection-2").select2({
-			minimumResultsForSearch: 20,
-			dropdownParent: $('#dropDownSelect2')
-		});
-	</script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
-
-</body>
+	</body>
 </html>
