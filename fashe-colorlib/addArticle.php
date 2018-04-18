61dcb33d4cad8c59a75886c109d340b2e1fac3f4
Jérémy <jeremy.gfeller@cpnv.ch>
2018-04-18 11:12:51 +0200

    Ajout d'un nouveau footer sur toutes les pages

--- fashe-colorlib/addArticle.php
+++ fashe-colorlib/addArticle.php
@@ -128,338 +128,73 @@
 	<!-- Cart -->
 	<section class="cart bgwhite p-t-70 p-b-100">
 		<div class="container">
-			<!-- Cart item -->
-			<!--div class="container-table-cart pos-relative">
-				<div class="wrap-table-shopping-cart bgwhite"-->
-                    <?php
-                        echo "
-                            <div class='row'>
-                                <div class='col-md-3 p-b-30'></div>
-                                <div class='col-md-6 p-b-30'>
-                                    <form method='post' class='leave-comment' enctype='multipart/form-data'>
-                                        Première illustration: <input type='file' name='illustration' style='margin-bottom: 15px;'><br>
-                                        Deuxième illustration: <input type='file' name='illustration1' style='margin-bottom: 15px;'><br>
-                                        Troisième illustration: <input type='file' name='illustration2'><br><br>
-                                        
-                                        <div class='bo4 of-hidden size15 m-b-20'>
-                                            <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newBrand' placeholder='Entrer la marque' required>
-                                        </div>
-
-                                        <div class='bo4 of-hidden size15 m-b-20'>
-                                            <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newModel' placeholder='Entrer le modèle'>
-                                        </div>
-                                        
-                                        Taille <br>
-                                        <select name='taille' id='taille' style='padding-right: 175px; margin-top: 10px; margin-bottom: 15px;'/>";
-                                            $query = "SELECT id_size, size FROM size;";
-                                            $sizes = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-                                            while($size = $sizes->fetch()) //fetch = aller chercher
-                                            {
-                                                extract($size); //$id_size, $size
-                                                echo "<option value='$id_size'>$size</option>";   
-                                            }
-                                        echo"
-                                        </select><br>
-                                        
-                                        Type d'article <br>
-                                        <select name='typeArticle' id='typeArticle' style='padding-right: 150px; margin-top: 10px; margin-bottom: 15px;'/>";
-                                            $query = "SELECT id_typeArticle, typeArticle FROM typearticle;";
-                                            $sizes = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-                                            while($size = $sizes->fetch()) //fetch = aller chercher
-                                            {
-                                                extract($size); //$id_typeArticle, $typeArticle
-                                                echo "<option value='$id_typeArticle'>$typeArticle</option>";   
-                                            }
-                                        echo"
-                                        </select>
-
-
-                                        <div class='bo4 of-hidden size15 m-b-20'>
-                                            <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newPrix' placeholder='Entrer le prix'>
-                                        </div>
-                                        
-                                        <div class='bo4 of-hidden size15 m-b-20'>
-                                            <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newColor' placeholder='Entrer la couleur'>
-                                        </div>
-                                        
-                                        <div class='bo4 of-hidden size15 m-b-20'>
-                                            <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newQuantity' placeholder='Entrer la quantité'>
-                                        </div>
-                                        
-                                        <div class='w-size25'>
-                                            <!-- Button -->
-                                            <button class='flex-c-m size2 bg1 bo-rad-23 hov1 m-text3 trans-0-4' name='create'>
-                                                Envoyer
-                                            </button>
-                                        </div>
-                                    </form>
+            <?php
+                echo "
+                    <div class='row'>
+                        <div class='col-md-3 p-b-30'></div>
+                        <div class='col-md-6 p-b-30'>
+                            <form method='post' class='leave-comment' enctype='multipart/form-data'>
+                                Première illustration: <input type='file' name='illustration' style='margin-bottom: 15px;'><br>
+                                Deuxième illustration: <input type='file' name='illustration1' style='margin-bottom: 15px;'><br>
+                                Troisième illustration: <input type='file' name='illustration2'><br><br>
+                                
+                                <div class='bo4 of-hidden size15 m-b-20'>
+                                    <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newBrand' placeholder='Entrer la marque' required>
                                 </div>
-                            </div>";
-                    ?>
-					<!--table class="table-shopping-cart">
-						<tr class="table-head">
-							<th class="column-1">Images</th>
-							<th class="column-2">Marque</th>
-							<th class="column-3">Modèle</th>
-							<th class="column-4">Taille</th>
-							<th class="column-5">Couleur</th>
-                            <th class='column-6'>Prix</th>
-                            <th class='column-7'>Quantité</th>
-						</tr-->
-                        <?php
-                        /*
-                        echo "
-                        <form method='post'>
-                            <tr class='table-row'>
-                                <td class='column-1'>
-                                    <div class='cart-img-product b-rad-4 o-f-hidden'>
-                                        <img src=''>
-                                    </div>
-                                </td>
-                                <td class='column-2'>
-                                    <input type='text' name='newBrand' placeholder='Entrer la marque'/>
-                                </td>
-                                <td class='column-3'>
-                                    <input type='text' name='newModel' placeholder='Entrer le modèle'/>
-                                </td>
-                                <td class='column-4'>
-                                    <select name='taille' id='taille'/>"; 
-                                        $query = "SELECT id_size, size FROM size;";
-                                        $sizes = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
-                                        while($size = $sizes->fetch()) //fetch = aller chercher
-                                        {
-                                            extract($size); //$id_size, $size
-                                            echo "<option value='$id_size'>$size</option>";   
-                                        }
-                                    echo "
-                                    </select>
-                                </td>
-                                <td class='column-5'>Couleur</td>
-                                <td class='column-6'>Prix</td>
-                                <td class='column-7'>Quantité</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td>
-                                    <button type='submit' name='create' style='padding-left: 110px;'>
-                                        <img src='images/icons/plus.png' alt='IMG-PRODUCT'> 
+
+                                <div class='bo4 of-hidden size15 m-b-20'>
+                                    <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newModel' placeholder='Entrer le modèle'>
+                                </div>
+                                
+                                Taille <br>
+                                <select name='taille' id='taille' style='padding-right: 175px; margin-top: 10px; margin-bottom: 15px;'/>";
+                                    $query = "SELECT id_size, size FROM size;";
+                                    $sizes = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
+                                    while($size = $sizes->fetch()) //fetch = aller chercher
+                                    {
+                                        extract($size); //$id_size, $size
+                                        echo "<option value='$id_size'>$size</option>";   
+                                    }
+                                echo"
+                                </select><br>
+                                
+                                Type d'article <br>
+                                <select name='typeArticle' id='typeArticle' style='padding-right: 150px; margin-top: 10px; margin-bottom: 15px;'/>";
+                                    $query = "SELECT id_typeArticle, typeArticle FROM typearticle;";
+                                    $sizes = $dbh->query($query) or die ("SQL Error in:<br> $query <br>Error message:".$dbh->errorInfo()[2]);
+                                    while($size = $sizes->fetch()) //fetch = aller chercher
+                                    {
+                                        extract($size); //$id_typeArticle, $typeArticle
+                                        echo "<option value='$id_typeArticle'>$typeArticle</option>";   
+                                    }
+                                echo"
+                                </select>
+
+
+                                <div class='bo4 of-hidden size15 m-b-20'>
+                                    <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newPrix' placeholder='Entrer le prix'>
+                                </div>
+                                
+                                <div class='bo4 of-hidden size15 m-b-20'>
+                                    <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newColor' placeholder='Entrer la couleur'>
+                                </div>
+                                
+                                <div class='bo4 of-hidden size15 m-b-20'>
+                                    <input class='sizefull s-text7 p-l-22 p-r-22' type='text' name='newQuantity' placeholder='Entrer la quantité'>
+                                </div>
+                                
+                                <div class='w-size25'>
+                                    <!-- Button -->
+                                    <button class='flex-c-m size2 bg1 bo-rad-23 hov1 m-text3 trans-0-4' name='create'>
+                                        Envoyer
                                     </button>
-                                </td>
-                            </tr>
-                        </form>"; */
-                    ?>
-					<!--/table>
-				</div>
-			</div-->
+                                </div>
+                            </form>
+                        </div>
+                    </div>";
+            ?>
 		</div>
 	</section>
-
-	<!-- Footer -->
-	<footer class="bg6 p-t-45 p-b-43 p-l-45 p-r-45">
-		<div class="flex-w p-b-90">
-			<div class="w-size6 p-t-30 p-l-15 p-r-15 respon3">
-				<h4 class="s-text12 p-b-30">
-					GET IN TOUCH
-				</h4>
-
-				<div>
-					<p class="s-text7 w-size27">
-						Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
-					</p>
-
-					<div class="flex-m p-t-30">
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-facebook"></a>
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-instagram"></a>
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-pinterest-p"></a>
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-snapchat-ghost"></a>
-						<a href="#" class="fs-18 color1 p-r-20 fa fa-youtube-play"></a>
-					</div>
-				</div>
-			</div>
-
-			<div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
-				<h4 class="s-text12 p-b-30">
-					Categories
-				</h4>
-
-				<ul>
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Men
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Women
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Dresses
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Sunglasses
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
-				<h4 class="s-text12 p-b-30">
-					Links
-				</h4>
-
-				<ul>
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Search
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							About Us
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Contact Us
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Returns
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div class="w-size7 p-t-30 p-l-15 p-r-15 respon4">
-				<h4 class="s-text12 p-b-30">
-					Help
-				</h4>
-
-				<ul>
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Track Order
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Returns
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							Shipping
-						</a>
-					</li>
-
-					<li class="p-b-9">
-						<a href="#" class="s-text7">
-							FAQs
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div class="w-size8 p-t-30 p-l-15 p-r-15 respon3">
-				<h4 class="s-text12 p-b-30">
-					Newsletter
-				</h4>
-
-				<form>
-					<div class="effect1 w-size9">
-						<input class="s-text7 bg6 w-full p-b-5" type="text" name="email" placeholder="email@example.com">
-						<span class="effect1-line"></span>
-					</div>
-
-					<div class="w-size2 p-t-20">
-						<!-- Button -->
-						<button class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
-							Subscribe
-						</button>
-					</div>
-
-				</form>
-			</div>
-		</div>
-
-		<div class="t-center p-l-15 p-r-15">
-			<a href="#">
-				<img class="h-size2" src="images/icons/paypal.png" alt="IMG-PAYPAL">
-			</a>
-
-			<a href="#">
-				<img class="h-size2" src="images/icons/visa.png" alt="IMG-VISA">
-			</a>
-
-			<a href="#">
-				<img class="h-size2" src="images/icons/mastercard.png" alt="IMG-MASTERCARD">
-			</a>
-
-			<a href="#">
-				<img class="h-size2" src="images/icons/express.png" alt="IMG-EXPRESS">
-			</a>
-
-			<a href="#">
-				<img class="h-size2" src="images/icons/discover.png" alt="IMG-DISCOVER">
-			</a>
-
-			<div class="t-center s-text8 p-t-20">
-				Copyright © 2018 All rights reserved. | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-			</div>
-		</div>
-	</footer>
-
-
-
-	<!-- Back to top -->
-	<div class="btn-back-to-top bg0-hov" id="myBtn">
-		<span class="symbol-btn-back-to-top">
-			<i class="fa fa-angle-double-up" aria-hidden="true"></i>
-		</span>
-	</div>
-
-	<!-- Container Selection -->
-	<div id="dropDownSelect1"></div>
-	<div id="dropDownSelect2"></div>
-
-
-
-<!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/bootstrap/js/popper.js"></script>
-	<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/select2/select2.min.js"></script>
-	<script type="text/javascript">
-		$(".selection-1").select2({
-			minimumResultsForSearch: 20,
-			dropdownParent: $('#dropDownSelect1')
-		});
-
-		$(".selection-2").select2({
-			minimumResultsForSearch: 20,
-			dropdownParent: $('#dropDownSelect2')
-		});
-	</script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
-
+    <?php require_once('footer.php'); ?>
 </body>
 </html>
