f8b1fba20dc76a60ed44cf391e46c7545ca88469
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-04-18 11:22:31 +0200

    Ajout d'un fichier pour le footer
    J'ai mis le même footer sur chacune des pages de mon site

---
+++ fashe-colorlib/footer.php
@@ -0,0 +1,8 @@
+<!-- Footer -->
+<footer class="bg6 p-t-45 p-b-43 p-l-45 p-r-45">
+    <div class="w-size8 p-t-30 p-l-15 p-r-15 respon4">
+        <h4 class="s-text12 p-b-30">
+            ZIRA - PRé-TPI
+        </h4>
+    </div>
+</footer>
